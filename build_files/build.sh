89b016f933e1570007be9e8ff37541bfd3121b80
FindMyLinux-Admin <fabio.marsmann01@gmail.com>
2026-04-21 21:12:56 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-### Install packages
+# 1. System-Programme (NordVPN & Brave Browser) installieren
+wget -qO - https://repo.nordvpn.com/gpg/nordvpn_public.asc | rpm --import -
+curl -s -o /etc/yum.repos.d/nordvpn.repo https://repo.nordvpn.com/yum/nordvpn/centos/x86_64/nordvpn.repo
+dnf5 install -y nordvpn
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+curl -s -o /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+dnf5 install -y brave-browser
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# 2. Der "Distro-Magic" Trick für deine restlichen Apps
+# Erstellt einen automatischen Installer, der beim ersten Hochfahren alles herunterlädt
 
-#### Example for enabling a System Unit File
+cat << 'EOF' > /usr/bin/install-my-apps
+#!/bin/bash
+# Installiere die Apps, wenn es noch nicht passiert ist
+if [ ! -f /var/opt/my-apps-installed ]; then
+    flatpak install -y flathub com.discordapp.Discord com.spotify.Client org.freecad.FreeCAD com.nordpass.NordPass io.github.enginkirmaci.lumux org.prismlauncher.PrismLauncher
+    touch /var/opt/my-apps-installed
+fi
+EOF
+chmod +x /usr/bin/install-my-apps
 
-systemctl enable podman.socket
+# Sagt dem System, dass es das Skript heimlich beim Booten starten soll
+cat << 'EOF' > /usr/lib/systemd/system/install-my-apps.service
+[Unit]
+Description=Installiere meine Custom Apps beim ersten Start
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/install-my-apps
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable install-my-apps.service
