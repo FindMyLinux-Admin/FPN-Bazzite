3bd649b49abf08dc49e37e6a6fff73fdb5949d16
FindMyLinux-Admin <fabio.marsmann01@gmail.com>
2026-04-21 22:13:12 +0200

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,30 +1,22 @@
 #!/bin/bash
 set -ouex pipefail
 
-# 1. System-Programme (NordVPN & Brave Browser) installieren
-wget -qO - https://repo.nordvpn.com/gpg/nordvpn_public.asc | rpm --import -
+# 1. System-Programme (nur NordVPN, da es tiefe Rechte braucht)
+curl -s https://repo.nordvpn.com/gpg/nordvpn_public.asc | rpm --import -
 curl -s -o /etc/yum.repos.d/nordvpn.repo https://repo.nordvpn.com/yum/nordvpn/centos/x86_64/nordvpn.repo
-dnf5 install -y nordvpn
+rpm-ostree install nordvpn
 
-rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-curl -s -o /etc/yum.repos.d/brave-browser.repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf5 install -y brave-browser
-
-
-# 2. Der "Distro-Magic" Trick für deine restlichen Apps
-# Erstellt einen automatischen Installer, der beim ersten Hochfahren alles herunterlädt
-
+# 2. Der "Distro-Magic" Trick für DEINE Apps (jetzt inkl. Brave Browser)
 cat << 'EOF' > /usr/bin/install-my-apps
 #!/bin/bash
-# Installiere die Apps, wenn es noch nicht passiert ist
 if [ ! -f /var/opt/my-apps-installed ]; then
-    flatpak install -y flathub com.discordapp.Discord com.spotify.Client org.freecad.FreeCAD com.nordpass.NordPass io.github.enginkirmaci.lumux org.prismlauncher.PrismLauncher
+    flatpak install -y flathub com.brave.Browser com.discordapp.Discord com.spotify.Client org.freecad.FreeCAD com.nordpass.NordPass io.github.enginkirmaci.lumux org.prismlauncher.PrismLauncher
     touch /var/opt/my-apps-installed
 fi
 EOF
 chmod +x /usr/bin/install-my-apps
 
-# Sagt dem System, dass es das Skript heimlich beim Booten starten soll
+# Sagt dem System, dass es das Skript beim Booten starten soll
 cat << 'EOF' > /usr/lib/systemd/system/install-my-apps.service
 [Unit]
 Description=Installiere meine Custom Apps beim ersten Start
